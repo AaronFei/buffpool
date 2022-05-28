592486e4e347310ba78db8b5a1118f66dfa54b7e
Aaron Fei <afei@marvell.com>
2022-05-28 19:55:46 +0800

    First

--- buf.go
+++ buf.go
@@ -1,21 +1,47 @@
 package simplebuf
 
-const sizeOfDescription = 4
-
-func InitBufferList(bufferSize int, bufferCount int) [][]byte {
-	bufferList := [][]byte{}
-	for i := 0; i < bufferCount; i++ {
-		bufferList = append(bufferList, make([]byte, sizeOfDescription+bufferSize))
-	}
-
-	return bufferList
+type BufferInfo_t struct {
+	list    chan []byte
+	current []byte
 }
 
-func SetLength(buf []byte, len uint) {
-	buf[0] = byte(len)
-	buf[1] = byte(len >> 8)
-	buf[2] = byte(len >> 16)
-	buf[3] = byte(len >> 24)
+const sizeOfDescription = 4
+
+func InitBufferList(bufferSize int, bufferCount int) BufferInfo_t {
+	bufferInfo := BufferInfo_t{
+		list: make(chan []byte, bufferCount),
+	}
+
+	for i := 0; i < bufferCount; i++ {
+		bufferInfo.list <- make([]byte, sizeOfDescription+bufferSize)
+	}
+
+	return bufferInfo
+}
+
+func (b *BufferInfo_t) Next() []byte {
+	select {
+	case buf := <-b.list:
+		b.current = buf
+		return buf[sizeOfDescription:]
+	default:
+		return nil
+	}
+}
+
+func (b *BufferInfo_t) Tail(buf []byte) {
+	b.list <- buf
+}
+
+func (b *BufferInfo_t) SetLength(len uint) {
+	b.current[0] = byte(len)
+	b.current[1] = byte(len >> 8)
+	b.current[2] = byte(len >> 16)
+	b.current[3] = byte(len >> 24)
+}
+
+func (b *BufferInfo_t) GetLength() uint {
+	return uint(b.current[0]) + uint(b.current[1])<<8 + uint(b.current[2])<<16 + uint(b.current[3])<<24
 }
 
 func GetLength(buf []byte) uint {
